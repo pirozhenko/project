97c87a413698ec6dd35734f8170679a1095eafcf
pirozhenko Alexandr <pirog_13@ukr.net>
2016-07-03 11:23:55 +0300

    add font-icon and correct style

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,40 @@ var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     autoprefixer = require('autoprefixer'),
     browser      = require('browser-sync'),
-    sourcemaps   = require('gulp-sourcemaps');
+    sourcemaps   = require('gulp-sourcemaps'),
+    iconfont     = require("gulp-iconfont"),
+    consolidate  = require("gulp-consolidate");
 
 gulp.task('sass', function () {
-  return gulp.src('sass/**/*.scss')
+  return gulp.src('scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css'))
         .pipe(browser.stream({match: '**/*.css'}))
         // .pipe(postcss([ autoprefixer({ browsers: ['last 3 version'] }) ]));
+});
+
+gulp.task("build:icons", function() {
+    return gulp.src(["./icons/*.svg"])//path to svg icons
+      .pipe(iconfont({
+        fontName: "myicons",
+        formats: ["ttf", "eot", "woff", "svg"],
+        centerHorizontally: true,
+        fixedWidth: true,
+        normalize: true
+      }))
+      .on("glyphs", function (glyphs) {
+
+        gulp.src("./icons/util/*.scss") // Template for scss files
+            .pipe(consolidate("lodash", {
+                glyphs: glyphs,
+                fontName: "myicons",
+                fontPath: "../fonts/"
+            }))
+            .pipe(gulp.dest("./scss/icons/"));//generated scss files with classes
+      })
+      .pipe(gulp.dest("./fonts/"));//icon font destination
 });
 
 // Starts a BrowerSync instance
@@ -26,6 +50,6 @@ gulp.task('serve', ['sass'], function(){
 
 // Runs all of the above tasks and then waits for files to change
 gulp.task('default', ['serve'], function() {
-  gulp.watch(['sass/**/*.scss'], ['sass']);
+  gulp.watch(['scss/**/*.scss'], ['sass']);
   gulp.watch('./**/*.html').on('change', browser.reload);
 });
